ccf64a5597dfbf7edaa841f12213f369cccf649d
AdeFuaad <adesholafuaad@gmail.com>
2023-04-18 11:57:06 -0400

    3D Shirt

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { CustomButton } from "../components";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./logo.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                MAKE <br className="xl:block hidden" /> IT YOURS.
+              </h1>
+            </motion.div>
+
+            <motion.div {...headTextAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Design your distinguished t-shirt design with the 3D
+                customization tool. <strong>Visualize your architecture</strong>{" "}
+                and own it.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
